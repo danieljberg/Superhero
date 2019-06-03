1061240fb78d9c020151b472578f387a4cbaf42a
unknown <mr.danieljberg@gmail.com>
2019-06-03 09:37:55 -0500

    Finished all CRUD opperations

--- Superhero/Controllers/SuperherosController.cs
+++ Superhero/Controllers/SuperherosController.cs
@@ -17,26 +17,26 @@ namespace Superhero.Controllers
         // GET: Superheros
         public ActionResult Index()
         {
-            Hero superheroList = new Hero();
-            return View(superheroList);
+            return View(context.Superheroes);
         }
 
         // GET: Superheros/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var superhero = context.Superheroes.Find(id);
+            return View(superhero);
         }
 
         // GET: Superheros/Create
         public ActionResult Create()
         {
-            Models.Hero superhero = new Models.Hero();
+            Models.Superhero superhero = new Models.Superhero();
             return View(superhero);
         }
 
         // POST: Superheros/Create
         [HttpPost]
-        public ActionResult Create(Models.Hero superhero)
+        public ActionResult Create(Models.Superhero superhero)
         {
             try
             {
@@ -54,18 +54,24 @@ namespace Superhero.Controllers
         // GET: Superheros/Edit/5
         public ActionResult Edit(int id)
         {
-            context.Superheroes.Find(id);
-            return View();
+            var superheroToEdit = context.Superheroes.Find(id);
+            return View(superheroToEdit);
         }
 
         // POST: Superheros/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, Models.Superhero superhero)
         {
             try
             {
-                // TODO: Add update logic here
-
+                // TODO: Add update logic here                
+                var superheroToEdit = context.Superheroes.Find(id);
+                superheroToEdit.name = superhero.name;
+                superheroToEdit.alterEgo = superhero.alterEgo;
+                superheroToEdit.primarySuperheroAbility = superhero.primarySuperheroAbility;
+                superheroToEdit.secondarySuperheroAbility = superhero.secondarySuperheroAbility;
+                superheroToEdit.catchphrase = superhero.catchphrase;
+                context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
@@ -77,17 +83,20 @@ namespace Superhero.Controllers
         // GET: Superheros/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var superheroToDelete = context.Superheroes.Find(id);
+            return View(superheroToDelete);
         }
 
         // POST: Superheros/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(int id, Models.Superhero superhero)
         {
             try
             {
                 // TODO: Add delete logic here
-
+                var superheroToDelete = context.Superheroes.Find(id);
+                context.Superheroes.Remove(superheroToDelete);
+                context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
--- Superhero/Models/Superhero.cs
+++ Superhero/Models/Superhero.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace Superhero.Models
 {
-    public class Hero
+    public class Superhero
     {
         [Key]  
         public int Id { get; set; }
